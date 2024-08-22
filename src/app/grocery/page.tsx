e3221a0a30c5e0a7d97c2e230e6ad14de4118254
aryallaxman99 <aryallaxman99@gmail.com>
2024-08-22 14:09:26 +0545

    working on researchSynthesis component

--- src/app/grocery/page.tsx
+++ src/app/grocery/page.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import HRMHeader from "@/Components/ReUsableComp/HRMHeader";
 import TextComponent from "@/Components/HRM/TextComponent";
 import ProjectGoalComponent from "@/Components/HRM/ProjectGoalComponent";
 import Research from "@/Components/Research/Research";
 import LogoComponent from "@/Components/Logo/LogoComponent";
+import ResearchSynthesis from "@/Components/Research/ResearchSynthesis";
 
 import groceryText from "@/texts/grocery.json";
 import groceryProjectGoals from "@/texts/groceryProjectGoals.json";
 import researchObjectives from "@/texts/researchObjectives.json";
+import groceryHoddingBord from "@/assets/groceryHoddingBoard.jpg";
 
 const page = () => {
   return (
@@ -36,6 +39,12 @@ const page = () => {
           text={researchObjectives}
         />
       </div>
+      <ResearchSynthesis />
+      <Image
+        src={groceryHoddingBord}
+        alt="hodding board"
+        className="h-auto w-full px-4 sm:px-6 md:px-8 lg:px-9 my-16 sm:my-24 md:my-32 lg:my-20"
+      />
     </div>
   );
 };
